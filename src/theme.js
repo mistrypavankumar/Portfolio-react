e7284214aa13c92d67a0d1f649a8881166f9460d
Mistry Pavan Kumar <68140538+mg143pavankumar@users.noreply.github.com>
2020-12-17 16:22:40 +0530

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@
 export const blueTheme = {
   body: "#EDF9FE",
   text: "#001C55",
-  highlight: "#A6E1FA",
+//   highlight: "#A6E1FA",
   dark: "#00072D",
   secondaryText: "#7F8DAA",
   imageHighlight: "#0E6BA8",
